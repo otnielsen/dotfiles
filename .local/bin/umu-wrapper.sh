e81e19789c9240fe80937f680ccd1a78283c10b0
olni0002 <olni0002@stud.kea.dk>
2025-07-18 19:10:04 +0200

    Add umu wrapper script and envvars

---
+++ .local/bin/umu-wrapper.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+. ~/.local/share/scripts/umu-env.sh
+exec gamemoderun umu-run "$@"
---
+++ .local/share/scripts/umu-env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+export RADV_TEX_ANISO=16
+export WINE_FULLSCREEN_INTEGER_SCALING=1
+export PROTONPATH=proton-cachyos
+export VK_DRIVER_FILES=~/.local/share/vulkan/icd.d/radeon_icd.x86_64.json:/usr/share/vulkan/icd.d/radeon_icd.i686.json
+export PROTON_ENABLE_WAYLAND=1
+export PROTON_USE_NTSYNC=1
