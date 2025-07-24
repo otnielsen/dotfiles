a24dd364a5d790422f8ac77d032e803bf5878864
olni0002 <olni0002@stud.kea.dk>
2025-07-24 17:25:13 +0200

    Move environment variables to umu-wrapper script so separate script isn't needed
    Also VK_DRIVER_FILES envvar isn't needed here anymore as it is set for the entire desktop in bash_profile

--- .local/bin/umu-wrapper.sh
+++ .local/bin/umu-wrapper.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
-. ~/.local/share/scripts/umu-env.sh
+export RADV_TEX_ANISO=16
+export WINE_FULLSCREEN_INTEGER_SCALING=1
+export PROTONPATH=proton-cachyos
+export PROTON_ENABLE_WAYLAND=1
+export PROTON_USE_NTSYNC=1
+
 exec gamemoderun umu-run "$@"
