43d529e2c5c24e87d328bb06b7aaa1e8bcd20428
olni0002 <olni0002@stud.kea.dk>
2025-07-26 12:53:34 +0200

    Add mangohud with config and build dependencies

--- .local/bin/umu-wrapper.sh
+++ .local/bin/umu-wrapper.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+export MANGOHUD=1
 export RADV_TEX_ANISO=16
 export WINE_FULLSCREEN_INTEGER_SCALING=1
 export PROTONPATH=proton-cachyos
 export PROTON_ENABLE_WAYLAND=1
 export PROTON_USE_NTSYNC=1
+export PROTON_USE_WOW64=1
 
 exec gamemoderun umu-run "$@"
