9f8def278a454deb3c3d197add75afc3d61b5bff
olni0002 <olni0002@stud.kea.dk>
2025-08-10 22:03:59 +0200

    Hide mouse cursor when running gamemode

--- .local/bin/gamemode-check.sh
+++ .local/bin/gamemode-check.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env sh
+#!/bin/dash
 
-set -e
+set -eu
 
 # get clock speed first so retrieval of information doesn't affect it
 clock_speed=$(grep MHz /proc/cpuinfo)
 
 gamemoded -s
 scxctl get
+echo "hidecursor loaded? $(qdbus org.kde.KWin /Effects isEffectLoaded hidecursor)"
 
 echo '#### governor ####'
 cat /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
