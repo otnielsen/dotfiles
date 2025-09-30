135902706964b2944f5df322ca541d8c846ad13d
olni0002 <olni0002@stud.kea.dk>
2025-09-30 09:45:00 +0200

    Only update qBittorrent port if vpn port changed

--- .local/share/scripts/vpn/portforward.sh
+++ .local/share/scripts/vpn/portforward.sh
@@ -7,7 +7,11 @@ while true; do
     if fping -q $gateway; then
         natpmpc -g $gateway -a 1 0 tcp
         listen_port=$(natpmpc -g $gateway -a 1 0 udp | awk '/^Mapped public port/ { print $4; exit }')
-        curl --no-progress-meter -d "json={\"listen_port\": $listen_port}" http://localhost:8080/api/v2/app/setPreferences
+
+        # only update qbittorrent listening port if it differs from the mapping by natpmpc
+        if [ "$(curl --no-progress-meter http://localhost:8080/api/v2/app/preferences | jq .listen_port)" != "$listen_port" ]; then
+            curl --no-progress-meter -d "json={\"listen_port\": $listen_port}" http://localhost:8080/api/v2/app/setPreferences
+        fi
     fi
     sleep 45
 done
