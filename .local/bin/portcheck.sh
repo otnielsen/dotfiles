043bafbc7167bacade437053997b6dc5cc0a31ea
olni0002 <olni0002@stud.kea.dk>
2025-09-30 17:24:54 +0200

    Add a port checker script
    Checks whether qBittorrent is reachable on its currently configured
    listening port

---
+++ .local/bin/portcheck.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -eu
+
+server_local_ip='192.168.1.213'
+server_public_ip=$(ssh jellyfin@$server_local_ip 'curl --no-progress-meter https://am.i.mullvad.net/ip')
+qbittorrent_port=$(curl --no-progress-meter http://${server_local_ip}:8080/api/v2/app/preferences | jq .listen_port)
+
+nmap -Pn -p "$qbittorrent_port" "$server_public_ip"
