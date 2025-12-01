77863926499712ee2e3d5c26b66ab7dab2d052a5
otnielsen <oliver.thor.nielsen100@gmail.com>
2025-12-01 15:51:41 +0100

    gamemode: change epp to performance but keep governor at powersave

--- .config/scripts/system/usr/local/libexec/gamemode-epp.sh
+++ .config/scripts/system/usr/local/libexec/gamemode-epp.sh
@@ -6,6 +6,11 @@ set -eu
 # preference, but when gamemode ends it doesn't restore the epp to
 # balance_performance
 
+case "$1" in
+gaming) pref=performance ;;
+default) pref=default ;;
+esac
+
 for epp in /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference; do
-    echo balance_performance >"$epp"
+    echo "$pref" >"$epp"
 done
