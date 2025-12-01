836df42f4560629743d59f14b044646d281be2e3
otnielsen <oliver.thor.nielsen100@gmail.com>
2025-12-01 15:53:56 +0100

    don't list epp and governor for every processor

--- .local/bin/gamemode-check.sh
+++ .local/bin/gamemode-check.sh
@@ -1,16 +1,21 @@
-#!/bin/dash
+#!/bin/sh
 
 set -eu
 
 # get clock speed first so retrieval of information doesn't affect it
-clock_speed=$(grep MHz /proc/cpuinfo)
+clock_speed=$(grep 'cpu MHz' /proc/cpuinfo)
 
 gamemoded -s
 scxctl get
-echo "hidecursor loaded? $(busctl --user call org.kde.KWin /Effects org.kde.kwin.Effects isEffectLoaded s hidecursor | awk '{ print $2 }')"
 
-echo '#### governor ####'
-cat /sys/devices/system/cpu/cpufreq/policy*/scaling_governor
+printf 'hidecursor loaded? '
+busctl --user call org.kde.KWin /Effects org.kde.kwin.Effects isEffectLoaded s hidecursor | awk '{ print $2 }'
+
+printf 'governor: '
+cat /sys/devices/system/cpu/cpufreq/policy*/scaling_governor | sort -u
+
+printf 'epp: '
+cat /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference | sort -u
 
 echo '#### cpu clock ####'
-echo "$clock_speed"
+echo "$clock_speed" | sort | uniq -c
