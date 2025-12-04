644ad0748be625e76dca2835abe22ae0737f1f33
otnielsen <oliver.thor.nielsen100@gmail.com>
2025-12-04 20:30:22 +0100

    use passive amd-pstate mode by default and guided when gaming

--- .config/scripts/system/usr/local/libexec/gamemode-epp.sh
+++ .config/scripts/system/usr/local/libexec/gamemode-epp.sh
@@ -6,11 +6,21 @@ set -eu
 # preference, but when gamemode ends it doesn't restore the epp to
 # balance_performance
 
+# case "$1" in
+# gaming) pref=performance ;;
+# default) pref=power ;;
+# esac
+#
+# for epp in /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference; do
+#     echo "$pref" >"$epp"
+# done
+
+# change pstate mode but not governor. guided is better for gaming, while
+# passive is better for energy saving during normal use
 case "$1" in
-gaming) pref=performance ;;
-default) pref=power ;;
+gaming) pref=guided ;;
+default) pref=passive ;;
+*) exit 1 ;;
 esac
 
-for epp in /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference; do
-    echo "$pref" >"$epp"
-done
+echo "$pref" >/sys/devices/system/cpu/amd_pstate/status
