633ed2ac628197c6913017cd42c339e00cd46201
otnielsen <oliver.thor.nielsen100@gmail.com>
2025-12-06 16:27:20 +0100

    Use amd p-state active with balance_power by default and performance in gamemode

--- .config/scripts/system/usr/local/libexec/gamemode-epp.sh
+++ .config/scripts/system/usr/local/libexec/gamemode-epp.sh
@@ -2,25 +2,11 @@
 
 set -eu
 
-# When governor is changed to performance so is the energy performance
-# preference, but when gamemode ends it doesn't restore the epp to
-# balance_performance
-
-# case "$1" in
-# gaming) pref=performance ;;
-# default) pref=power ;;
-# esac
-#
-# for epp in /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference; do
-#     echo "$pref" >"$epp"
-# done
-
-# change pstate mode but not governor. guided is better for gaming, while
-# passive is better for energy saving during normal use
 case "$1" in
-gaming) pref=guided ;;
-default) pref=passive ;;
-*) exit 1 ;;
+gaming) pref=performance ;;
+balanced) pref=balance_power ;;
 esac
 
-echo "$pref" >/sys/devices/system/cpu/amd_pstate/status
+for epp in /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference; do
+    echo "$pref" >"$epp"
+done
--- .local/bin/gamemode-check.sh
+++ .local/bin/gamemode-check.sh
@@ -11,5 +11,5 @@ busctl --user call org.kde.KWin /Effects org.kde.kwin.Effects isEffectLoaded s h
 printf 'governor: '
 cat /sys/devices/system/cpu/cpufreq/policy*/scaling_governor | sort -u
 
-printf 'amd-pstate: '
-cat /sys/devices/system/cpu/amd_pstate/status
+printf 'epp: '
+cat /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference | sort -u
