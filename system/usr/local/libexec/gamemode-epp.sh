010f465b62d139649e31c13edbc6600789fc1676
otnielsen <oliver.thor.nielsen100@gmail.com>
2026-08-03 14:41:29 +0200

    gamemode: change gpu power profile
    Use POWER_SAVING by default and 3D_FULL_SCREEN when gaming.

--- system/usr/local/libexec/gamemode-epp.sh
+++ system/usr/local/libexec/gamemode-epp.sh
@@ -3,10 +3,26 @@
 set -eu
 
 case "$1" in
-gaming) pref=performance ;;
-balanced) pref=balance_power ;;
+gaming)
+    pref=performance
+    profile=1
+    ;;
+balanced)
+    pref=balance_power
+    profile=2
+    ;;
 esac
 
 for epp in /sys/devices/system/cpu/cpufreq/policy*/energy_performance_preference; do
     echo "$pref" >"$epp"
 done
+
+card=$(udevadm trigger --dry-run --verbose \
+    --subsystem-match=pci \
+    --property-match=DRIVER=amdgpu \
+    --attr-match=unique_id=22190ab2d9cbbf14)
+profile_file="$card/pp_power_profile_mode"
+
+if [ -d "$card" ] && [ -f "$profile_file" ]; then
+    echo "$profile" >"$profile_file"
+fi
